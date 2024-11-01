63db0c858b24c0b736a765a36110fb755a951132
Vera Filimonova <filimonovavera96@gmail.com>
2024-11-01 14:38:48 +0300

    feat: resolve the task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    const codes = {
+        '10': '.',
+       '11': '-',
+        ' ': ' ',
+    }
+    
+    let lettersArr = [];
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        if (letter === '**********') {
+            letter = ' ';
+        } 
+    
+        let innerArr = [];    //поделили по 10 знакам letter = '0010101010'
+        for (let j = 0; j < letter.length; j += 2) {
+            innerArr.push(letter.slice(j, j + 2))
+        }
+        lettersArr.push(innerArr);
+    }
+    
+    let arrNoNull = [];                                                                //убираем 00 
+    for (let i = 0; i < lettersArr.length; i++) {
+        let innerArrNoNull = lettersArr[i].filter(symb => symb !== '00');
+        arrNoNull.push(innerArrNoNull);
+    }
+   
+// в точки запятые превратили
+    let codedArr = arrNoNull.map(innerArr => innerArr.map(item => item = codes[item]));
+    let newcodedArr = codedArr.map(item => item.join(''));
+    console.log(newcodedArr);
+    return newcodedArr.map(item => MORSE_TABLE[item]).join('');
 }
 
 module.exports = {
